02d5b64a8d108c0128fbf72afad578414c091a34
Rakesh Singh <bhaisingh@gmail.com>
2021-01-18 12:58:50 -0800

    Meme display completed.

---
+++ main.js
@@ -0,0 +1,32 @@
+var imageUrls = [
+'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
+'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+
+const gallery = document.querySelector('main');
+const imageUrlInput = document.querySelector('input');
+const addImageButton = document.querySelector('button');
+
+addImageButton.addEventListener('click', function (event) {
+    if (imageUrlInput.value !== '') {
+        imageUrls.push(imageUrlInput.value);
+    }
+    imageUrlInput.value = '';
+    updateGallery();
+});
+
+
+function updateGallery() {
+    gallery.innerHTML = '';
+   imageUrls.forEach(function(imageLink){
+       const imageElement = document.createElement('img');
+       imageElement.className = 'gallery-image';
+       imageElement.src = imageLink;
+       gallery.appendChild(imageElement);
+   })     
+   
+}
+
+updateGallery();
+ 
